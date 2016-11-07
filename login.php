65d45383b07f996fa74bc6bd1ab33ff74c4b5518
Sankkara Narayanan <Sankkara Narayanan>
2016-11-07 20:06:26 +0530

    initialization

---
+++ login.php
@@ -0,0 +1,14 @@
+<?php
+include 'dbconn.php';
+$url="https://www.facebook.com/";
+connect();
+echo "<p>Connected with the database</p>";
+
+
+disconnect();
+echo "<p>Disconnected from the database\n</p>";
+
+echo "<p>Site not yet ready</p>
+<p>So redirecting you to Facebook</p>";
+
+header("Refresh: 0; url= $url");
