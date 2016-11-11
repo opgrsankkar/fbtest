0350b40937a79117d62b46db0e43e399cc1083c9
Sankkara Narayanan <Sankkara Narayanan>
2016-11-11 15:55:17 +0530

    tab indices to index.php

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ if (session_status() == PHP_SESSION_NONE) {
             <span class="col-6-sm"></span>
             <span id="navbarname" class="col-1-sm"><?php echo $_SESSION['fname']?></span>
             <span class="col-1-sm"></span>
-            <span id="logoutbtn" class="col-1-sm"><a href="logout.php">!</a></span>
+            <span id="logoutbtn" class="col-1-sm"><a href="logout.php" tabindex="-1">!</a></span>
         </nav>
 		<div id="sidenav" class="sidenav">
             <span class=""><a href="#">Feed</a></span>
@@ -45,10 +45,10 @@ if (session_status() == PHP_SESSION_NONE) {
         <div class="middle-content">
             <div id="newposttextdiv">
                 <form id="addpostform" method="POST" action="addPost.php">
-                    <textarea id="newposttext" class="row" name="postcontent" 
+                    <textarea id="newposttext" class="row" name="postcontent" tabindex="1"
                                 onfocus="clearContents(this);"
                                 onblur="addContents(this)">What's on your mind...</textarea>
-                    <input class="btn row col-2-sm" type="submit" value="Post">
+                    <input class="btn row col-2-sm" type="submit" value="Post" tabindex="2">
                 </form>
             </div>
             <div>
