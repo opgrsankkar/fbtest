e6844e0933e077ea8696a78c59bfba6132d885d1
athipattuaravind <aravind.astronaut001@hotmail.com>
2016-11-14 21:21:36 +0530

    index.php with photo-display

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ if (session_status() == PHP_SESSION_NONE) {
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		</echo><link rel="stylesheet" type="text/css" href="simple-grid.css">
+		<link rel="stylesheet" type="text/css" href="simple-grid.css">
         <link rel="stylesheet" type="text/css" href="main.css">
         <script src="jquery-3.1.0.min.js"></script>
         <script>
@@ -34,7 +34,6 @@ if (session_status() == PHP_SESSION_NONE) {
 		<nav class="row">
             <span class="col-1-sm"></span>
             <span class="col-2-sm">F.R.I.E.N.D.S</span>
-        
             <span class="col-6-sm"></span>
             <span id="navbarname" class="col-1-sm"><?php echo $_SESSION['fname']?></span>
             <span class="col-1-sm"></span>
@@ -42,24 +41,12 @@ if (session_status() == PHP_SESSION_NONE) {
         </nav>
 		<div id="sidenav" class="sidenav">
             <span class=""><a href="#">Feed</a></span>
-			<span class=""><a href="posts.html">Posts</a></span>
-            <span class=""><a href="photos.html">Photos</a></span>
+			<span class=""><a href="posts.php">Posts</a></span>
+            <span class=""><a href="photos.php">Photos</a></span>
             <span class=""><a href="events.php">Events</a></span>
             <span class=""><a href="friends.php">Friends</a></span>
 		</div>
         <div class="middle-content">
-            <div id="newposttextdiv">
-                <form id="addpostform" method="POST" action="addPost.php" enctype="multipart/form-data">
-                    <textarea id="newposttext" class="row" name="postcontent" tabindex="1" placeholder="What's on your mind..."></textarea>
-                    <div id="post-upload-div" class="row">  
-                        <input class="btn col-2-sm" type="submit" value="Post" tabindex="2">
-                        <label class="fileContainer col-2-sm">
-                            <span class="upload-btn">Add Photos</span>
-                            <input type="file" name="uploaded[]" multiple/>
-                        </label>
-                    </div>
-                </form>
-            </div>
             <div id="posts">
                 <?php 
                     connect();
@@ -69,9 +56,9 @@ if (session_status() == PHP_SESSION_NONE) {
                     if ($result->num_rows > 0) {
                     	while($row_cursor = $result->fetch_assoc()) { ?>
 		                    <div class="box">
-                            <p> <span class="posted-by"><?php echo $row_cursor['fname'] ?> </span>
-                                <span class="posted-by"> <?php echo $row_cursor['lname'] ?> </span>
-                                <span class="posted-on"> <?php echo before($row_cursor['add_date']) ?></span>
+                            <p> <span class="posted-by"><?= $row_cursor['fname'] ?> </span>
+                                <span class="posted-by"> <?= $row_cursor['lname'] ?> </span>
+                                <span class="posted-on"> <?= before($row_cursor['add_date']) ?></span>
                                 <?php $post_id=$row_cursor['post_id']; ?>
                                 <span class="col-1">
                                     <select onchange="postOptions(this.value,<?= $post_id ?>)" class="post-options">
@@ -82,6 +69,17 @@ if (session_status() == PHP_SESSION_NONE) {
                                 </span>
                             </p>
                             <p id="post_content" class="row post"> <?php echo $row_cursor['content'] ?></p>
+                            <div id="photos-div" class="row">
+                                <?php 
+                                $sqlP = "SELECT photo_id,link FROM photos where post_id='".$post_id."'";
+                                $resultP = $conn->query($sqlP);
+                                if ($resultP->num_rows > 0) {
+                    	            while($row_cursorP = $resultP->fetch_assoc()) { ?>
+                                    <img class="photo-display" src="<?= 'photos/'.$row_cursorP['link'] ?>">
+                                <?php
+                                    }
+                                } ?>
+                            </div>
                             </div>
 	                    <?php 
                         }
