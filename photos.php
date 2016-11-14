ec562888ab09d824cc0bb87d553a8f6bba8009d7
Sankkara Narayanan <Sankkara Narayanan>
2016-11-14 21:58:41 +0530

    photos.php complete;

--- photos.php
+++ photos.php
@@ -5,6 +5,7 @@ if (session_status() == PHP_SESSION_NONE) {
     session_start();
 	if($_SESSION['fname']==''){header('Location:login.php');}
 }
+$user_id = $_SESSION['user_id'];
 ?>
 <!DOCTYPE html>
 <html>
@@ -41,41 +42,24 @@ if (session_status() == PHP_SESSION_NONE) {
             <span id="logoutbtn" class="col-1-sm"><a href="logout.php" tabindex="-1">!</a></span>
         </nav>
 		<div id="sidenav" class="sidenav">
-            <span class=""><a href="#">Feed</a></span>
+            <span class=""><a href="index.php">Feed</a></span>
 			<span class=""><a href="posts.php">Posts</a></span>
             <span class=""><a href="photos.php">Photos</a></span>
             <span class=""><a href="events.php">Events</a></span>
             <span class=""><a href="friends.php">Friends</a></span>
 		</div>
         <div class="middle-content">
-            <div id="photosdiv">
-                <?php 
-                    connect();
-                    $sql = 'SELECT post_id,fname,lname,content,UNIX_TIMESTAMP(add_date) AS add_date FROM post NATURAL JOIN users ORDER BY add_date DESC';
-                    $result = $conn->query($sql);
-
-                    if ($result->num_rows > 0) {
-                    	while($row_cursor = $result->fetch_assoc()) { ?>
-		                    <div class="box">
-                            <p> <span class="posted-by"><?php echo $row_cursor['fname'] ?> </span>
-                                <span class="posted-by"> <?php echo $row_cursor['lname'] ?> </span>
-                                <span class="posted-on"> <?php echo before($row_cursor['add_date']) ?></span>
-                                <?php $post_id=$row_cursor['post_id']; ?>
-                                <span class="col-1">
-                                    <select onchange="postOptions(this.value,<?= $post_id ?>)" class="post-options">
-                                        <option value=""></option>
-                                        <option value="deletePost">Delete Post</option></span>
-                                        <option value="editPost">Edit Post</option></span>
-                                    </select>
-                                </span>
-                            </p>
-                            <p id="post_content" class="row post"> <?php echo $row_cursor['content'] ?></p>
-                            </div>
-	                    <?php 
-                        }
+            <div id="photos-div" class="row">
+                <?php
+                connect();
+                $sqlP = "SELECT photo_id,link FROM photos where user_id = '".$user_id."'";
+                $resultP = $conn->query($sqlP);
+                if ($resultP->num_rows > 0) {
+                    while($row_cursorP = $resultP->fetch_assoc()) { ?>
+                    <img class="photo-display box" src="<?= 'photos/'.$row_cursorP['link'] ?>">
+                <?php
                     }
-                ?>
-                <?php disconnect(); ?> 
+                    } disconnect();?>
             </div>
         </div>
 	</body>
--- posts.php
+++ posts.php
@@ -5,6 +5,7 @@ if (session_status() == PHP_SESSION_NONE) {
     session_start();
 	if($_SESSION['fname']==''){header('Location:login.php');}
 }
+$user_id = $_SESSION[''];
 ?>
 <!DOCTYPE html>
 <html>
