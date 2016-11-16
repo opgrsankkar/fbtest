397e61d7612bfa7738f2ecc36dca379f313efbf9
sanjayramraj <sanjayramraj97@gmail.com>
2016-11-16 15:13:55 +0530

    friends.php completed

--- friends.php
+++ friends.php
@@ -29,5 +29,19 @@ if (session_status() == PHP_SESSION_NONE) {
             <span class=""><a href="events.php">Events</a></span>
 		</div>
         <div class="display-friends">
+            <div class="row">
+                <?php
+                    $friendext = "SELECT user_id1,user_id2 FROM RIENDS WHERE  user_id1='".$_SESSION['user_id']."'";
+                    $result = $conn->query($friendext);
+                    if($result->num_rows >0){
+                        while($row_cursor = $result->fetch_assoc()){
+                            $sel_result="SELECT user_id2 from friends where users.user_id=user_id2 and user_id1='".$_SESSION['user_id']."'";
+                            echo  $sel_result;
+                            echo " ";
+                        }
+                    }
+                 ?>   
+                 </div>
+            </div>        
     </body>
 </html>
